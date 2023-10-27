0e0faa23530a0fe788fb602016c193e6506de773
RatnaPriya-2 <ratnapriya.t@gmail.com>
2023-10-27 21:12:16 +0530

    minor changes

--- src/components/sections/reservePages/Heading.js
+++ src/components/sections/reservePages/Heading.js
@@ -1,8 +1,7 @@
-import HeroImage from "../../../assets/restaurant.jpg";
+
 export default function Heading() {
   return (
     <header className="reserve-table">
-      
       <div className="reserve-header-text">
         <h1>Reserve a table</h1>
       </div>
